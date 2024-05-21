8e21fc3993c7872fd9a76f4997e92eb39a1ae116
Antonio Martinez <tonhkw@Antonios-MacBook-Air.local>
2024-05-21 08:50:41 -0500

    improving ui of visualizations page

--- src/pages/Visualizations/Visualizations.jsx
+++ src/pages/Visualizations/Visualizations.jsx
@@ -4,30 +4,36 @@ import './Visualizations.styles.css'; // Import CSS file for styling
 function Visualizations() {
   return (
     <div className="tableau-visualizations-container">
-      <h1>Vizualizations</h1>
+      <h1>Visualizations</h1>
       <ul className="visualization-list">
         <li>
-          <a href="https://public.tableau.com/app/profile/antonio4234/viz/HouseSalesDashboard_17156295410160/HouseSalesDashboard" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://public.tableau.com/app/profile/antonio4234/viz/HouseSalesDashboard_17156295410160/HouseSalesDashboard"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="visualization-link"
+          >
             House Sales Dashboard
           </a>
           <p className="made-with">Made with Tableau</p>
-          <p>An interactive dataset for the House Sales of Washington County in the US.
-            Select the month from the dropdown to dynamically change the data. Select the day of
-            the month to change Distribution of prices and Condition Heatmap.
+          <p className="description">
+            An interactive dataset for the House Sales of Washington County in the US. Select the month from the dropdown to dynamically change the data. Select the day of the month to change Distribution of prices and Condition Heatmap.
           </p>
         </li>
         <li>
-          <a href="https://public.tableau.com/app/profile/antonio4234/viz/BritishAirwaysReview_17163023936000/BritishAirwaysReview" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://public.tableau.com/app/profile/antonio4234/viz/BritishAirwaysReview_17163023936000/BritishAirwaysReview"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="visualization-link"
+          >
             British Airways Review
           </a>
           <p className="made-with">Made with Tableau</p>
-          <p>
-            Pick a metric of the review like Overall Rating, Entertainment, Seat Comfort, etc to visualize in an interactiva
-            map the Average review for that selected metric. The interactive dashboard allows you to select between a set of different
-            metrics and even periods of times from Octuber 2016 to October 2023.
+          <p className="description">
+            Pick a metric of the review like Overall Rating, Entertainment, Seat Comfort, etc. to visualize in an interactive map the average review for that selected metric. The interactive dashboard allows you to select between different metrics and periods of time from October 2016 to October 2023.
           </p>
         </li>
-       
         {/* Add more dashboards as needed */}
       </ul>
     </div>
