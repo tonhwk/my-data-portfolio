04ecda7a69429d3a4553366d04bbfd3e6720ad6e
Antonio Martinez <tonhkw@Antonios-MacBook-Air.local>
2024-05-26 10:52:12 -0600

    fixing sql basic analysis link

--- src/pages/AdvancedSQL/AdvancedSQL.jsx
+++ src/pages/AdvancedSQL/AdvancedSQL.jsx
@@ -5,18 +5,18 @@ import Card from '../../components/ReusableCard'; // Import the Card component
 const projects = [
   {
     "image": "/images/sales_sql.jpg",
-    "title": "Advanced SQL Analysis",
+    "title": "Basic SQL Queries",
     "tools": ["SQL"],
     "projectTypes": ["Data Analysis"],
-    "link": "https://github.com/tonhwk/data_analysis_potfolio/blob/main/Advanced%20SQL%20Queries/Retail%20Sales%20Analysis/README.md",
-    "projectDescription": "This project showcases advanced SQL analysis techniques through a series of scripts designed for data analysis. The scripts cover basic data retrieval, complex joins, aggregations, and reporting. Users execute the scripts in sequence using a SQL editor to analyze datasets and extract insights. Example queries include basic data retrieval, joining multiple tables, aggregating data for summary statistics, and generating insightful reports and visualizations."
+    "link": "https://github.com/tonhwk/data_analysis_potfolio/blob/main/Advanced%20SQL%20Queries/Basic%20SQL%20Queries/README.md",
+    "projectDescription": "This project showcases basic SQL queries through a series of scripts designed for data analysis. The scripts cover basic data retrieval, joins, aggregations, and reporting. Users execute the scripts in sequence using a SQL editor to analyze datasets and extract insights. Example queries include basic data retrieval, joining multiple tables, aggregating data for summary statistics, and generating insightful reports and visualizations."
   }
 ]
 
 function AdvancedSQL() {
   return (
     <div className="advanced-sql-page">
-      <h2>Advanced SQL Analysis</h2>
+      <h2>SQL Analysis & Querying</h2>
       <div className="cards">
         {projects.map((project, index) => (
           <Card
