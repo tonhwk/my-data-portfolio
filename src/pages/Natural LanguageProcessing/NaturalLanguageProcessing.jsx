4b9b719d53b5bb33254dc2ffed59d7cb6437f55f
tonhwk <andres_btk@yahoo.com>
2024-06-25 23:10:09 -0600

    adding spam detection project

--- src/pages/Natural LanguageProcessing/NaturalLanguageProcessing.jsx
+++ src/pages/Natural LanguageProcessing/NaturalLanguageProcessing.jsx
@@ -5,12 +5,20 @@ import Card from '../../components/ReusableCard'; // Import the Card component
 const projects = [
     {
       "image": "/images/sentiment_analysis.jpeg",
-      "title": "Sentiment Analysis with NLP",
+      "title": "Sentiment Analysis",
       "tools": ["Python", "pandas", "scikit-learn", "NLTK"],
       "projectTypes": ["Text Analysis", "Machine Learning"],
       "link": "https://github.com/tonhwk/data_analysis_potfolio/blob/main/Natural%20Language%20Processing/nlp.py",
       "projectDescription": "This project focuses on sentiment analysis of IMDB reviews using Natural Language Processing techniques. It covers data loading, pre-processing, feature extraction using TF-IDF, and training a Naive Bayes classifier. The project demonstrates the process of converting textual data into meaningful insights by predicting the sentiment of movie reviews."
-    }
+    },
+    {
+      "image": "/images/spam_detector.jpg",
+      "title": "Spam Detector",
+      "tools": ["Python", "pandas", "scikit-learn", "NLTK", "Matplotlib", "joblib"],
+      "projectTypes": ["Text Classification", "Machine Learning"],
+      "link": "https://github.com/tonhwk/data_analysis_potfolio/blob/main/Natural%20Language%20Processing/spam_detector.py",
+      "projectDescription": "This project focuses on detecting spam messages using Natural Language Processing and machine learning techniques. It involves data loading, text preprocessing, feature extraction using TF-IDF, and training a Logistic Regression classifier. The project includes evaluating the model's performance through metrics like accuracy, classification report, confusion matrix, and ROC-AUC score. It also demonstrates how to save and reuse the trained model and vectorizer."
+  }
   ]
   
 
